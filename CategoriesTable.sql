ec5449351c3290fe690c512ffd29a2126c3416e4
vlnp13 <vlnp13@gmail.com>
2022-06-30 12:50:13 -0400

    Creating Categories Table and Inserting Values

---
+++ CategoriesTable.sql
@@ -0,0 +1,16 @@
+CREATE TABLE categories (
+CategoryID int(11) NOT NULL,
+CategoryName varchar(255) DEFAULT NULL,
+Description varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO categories (CategoryID, CategoryName, Description) VALUES
+(1, 'Beverages', 'Soft drinks, coffees, teas, beers, and ales'),
+(2, 'Condiments', 'Sweet and savory sauces, relishes, spreads, and seasonings'),
+(3, 'Confections', 'Desserts, candies, and sweet breads'),
+(4, 'Dairy Products', 'Cheeses'),
+(5, 'Grains/Cereals', 'Breads, crackers, pasta, and cereal'),
+(6, 'Meat/Poultry', 'Prepared meats'),
+(7, 'Produce', 'Dried fruit and bean curd'),
+(8, 'Seafood', 'Seaweed and fish');
+
